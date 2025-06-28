74037c1b9694f2e5b466ebd723480c506d90fd39
ACHAL DIWASE <achaldiwase93@gmail.com>
2025-06-28 14:47:59 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,10 @@
+a=float(input("enter the first number:"))
+b=float(input("enter the second number :"))
+add=a+b
+print("addition is :",add)
+sub=a-b
+print(" substraction is :",sub)
+div=a/b
+print("division is :",div)
+mul=a*b
+print("multiplication  is :",mul)
